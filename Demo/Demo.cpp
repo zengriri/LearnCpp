871222bc18b4295e701df091afe66b4add428547
Sundy <971213938@qq.com>
2022-03-22 21:09:56 +0800

    first commit

---
+++ Demo/Demo.cpp
@@ -0,0 +1,16 @@
+﻿#include <cstdint>
+#include <iostream>
+
+int main()
+{
+    std::int16_t x{}; // x is 16 bits, holds from -32768 to 32767
+    std::cout << "Enter a number between -32768 and 32767: ";
+    std::cin >> x;
+
+    std::int16_t y{}; // y is 16 bits, holds from -32768 to 32767
+    std::cout << "Enter another number between -32768 and 32767: ";
+    std::cin >> y;
+
+    std::cout << "The sum is: " << x + y << '\n';
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # LearnCpp
 cpp note
+
+# 参照：https://www.learncpp.com/   进行学习，归纳，总结
+# 开发工具:VsCode
+
