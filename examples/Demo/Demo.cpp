6b38930cc476ff660095bc9fbc01cb5d822af1c0
Sundy <971213938@qq.com>
2024-06-11 19:17:44 +0800

    type

--- examples/Demo/Demo.cpp
+++ examples/Demo/Demo.cpp
@@ -2,8 +2,8 @@
 
 int main()
 {
-    char ch{ 97 }; // 97 is ASCII code for 'a'
-    std::cout << ch << '\n';
+    int bin{ 0b1011'0010 };  // assign binary 1011 0010 to the variable
+    long value{ 2'132'673'462 }; // much easier to read than 2132673462
 
     return 0;
 }
